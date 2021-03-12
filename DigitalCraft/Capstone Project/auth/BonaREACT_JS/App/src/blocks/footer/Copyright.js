18bfb407dee7122eb720120585512441a9cc17cb
Kanny <kanny.ghafour87@gmail.com>
2021-03-12 17:04:32 -0600

    final before presention

--- DigitalCraft/Capstone Project/auth/BonaREACT_JS/App/src/blocks/footer/Copyright.js
+++ DigitalCraft/Capstone Project/auth/BonaREACT_JS/App/src/blocks/footer/Copyright.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Copyright = () => {
     return (
         <div className="copyright">
-            <p>© { new Date().getFullYear() } Health & Medical Application by <a href="https://www.kanny.us" target="_blank">Kanny Mohamad</a></p>
+            <p>© { new Date().getFullYear() } Health & Medical Application by <a href="http://www.kanny.us" target="_blank">Kanny Mohamad</a></p>
         </div>
     );
 };
